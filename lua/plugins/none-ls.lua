8c297a2b8dfb5350647b9b804d7374cd602635cd
Alex <alex@alexedwards.co>
2024-08-04 13:48:58 +0000

    feat(plugins): none-ls for lsp-like ops

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      local sources = opts.sources or {}
+      opts.sources = vim.list_extend(sources, {
+        nls.builtins.code_actions.gomodifytags,
+        nls.builtins.code_actions.impl,
+        nls.builtins.formatting.goimports,
+        nls.builtins.formatting.gofumpt,
+        nls.builtins.diagnostics.markdownlint,
+      })
+    end,
+  },
+}
